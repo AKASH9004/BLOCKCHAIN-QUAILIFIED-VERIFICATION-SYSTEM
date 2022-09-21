5afc146a76a88f7d2a72fea7f120164ed9e9e568
doshidev01 <doshidev01@gmail.com>
2022-09-21 19:20:50 +0530

    first commit

--- blockchain/src/components/registers/register.js
+++ blockchain/src/components/registers/register.js
@@ -1,27 +1,105 @@
 import "./register.css";
+import { useState, useEffect } from "react";
 import { useNavigate} from 'react-router-dom'; 
 
+function Register() {
+const initialValues = { username: "", email: "", password: "", confirmPassword: ""};
+const [formValues, setFormValues] = useState(initialValues);
+const [formErrors, setFormErrors] = useState({});
+const [isSubmit, setIsSubmit] = useState(false);
 
-const Register = () => {
-    
-    const navigate = useNavigate();
-        function redirectLogin(){ 
-            navigate('/login'); 
-        }
 
-    return(
-        <div className="register">
-            <h1>REGISTER</h1>
-            <input type="text" placeholder="Enter your Name"></input>
-            <input type="text" placeholder="Enter your Email"></input>
-            <input type="password" placeholder="Enter your Password"></input>
-            <input type="password" placeholder="Conform Password"></input>
-            <div className="button" >REGISTER</div>
-            <div>or</div>
-            <div className="button" onClick={redirectLogin}>LOGIN</div>
+const navigate = useNavigate();
+function redirectLogin(){ 
+    navigate('/login'); 
+};
+const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
 
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+    if (values.confirmPassword != values.password) {
+        errors.confirmpassword = "Password doesn't match";
+    }
+    return errors;
+  };
+
+  return(
+    <div className="register">
+            <form onSubmit={handleSubmit}>
+        <h1>Register Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formValues.username}
+            />
+            </div>
+          <p>{formErrors.username}</p>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formValues.email}
+            />
+            </div>
+          <p>{formErrors.email}</p>
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formValues.password}
+            />
+            </div>
+            <p>{formErrors.password}</p>
+            <div className="field">
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              placeholder="confirmPassword"
+              value={formValues.confirmPassword}
+            />
+            </div>
+          <p>{formErrors.password}</p>
+          <button className="fluid ui button blue">Submit</button>
         </div>
-    )
+      </form>
+        </div>
+    );
 }
 
 export default Register
